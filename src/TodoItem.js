00a39f209895f944ba7eefcfb1f57e0422b0e2c5
Francisco Mercado <fmercado00@hotmail.com>
2022-06-29 22:03:16 -0500

    Adding onComplete and onDelete functionality

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,16 +2,11 @@ import React from 'react';
 import './TodoItem.css';
 
 function TodoItem(props) {
-  const onComplete = () => {
-    alert('Ya completaste del todo ' +  props.text);
-  };
-  const onDelete = () => {
-    alert('Borraste el todo ' +  props.text);
-  };
+
   return (
     <li className="TodoItem">
       <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
-      onClick = {onComplete}
+      onClick = {props.onComplete}
       >
         √
       </span>
@@ -19,7 +14,7 @@ function TodoItem(props) {
         {props.text}
       </p>
       <span className="Icon Icon-delete"
-            onClick={onDelete}
+            onClick={props.onDelete}
       >
         X
       </span>
